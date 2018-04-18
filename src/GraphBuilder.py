49a6c43df0f3ba81fd4171632e4be5dfe5c6faff
ludovica <ellepannitto@gmail.com>
2018-04-18 22:41:59 +0200

    ver 0.6.3

--- src/GraphBuilder.py
+++ src/GraphBuilder.py
@@ -2,6 +2,7 @@ from py2neo import *
 import gzip
 from functools import lru_cache
 import math
+import re
 
 
 class GraphBuilder:
@@ -16,16 +17,20 @@ class GraphBuilder:
 		self.vocab_file = self.source_folder+"sorted."+self.corpus+".voc.gz"
 		self.struct_file = self.source_folder+"sorted."+self.corpus+".struct.gz" 
 		self.edges_file = self.source_folder+"sorted."+self.corpus+".edges.deprel.gz"
-		self.edges_generic_file = self.source_folder+"sorted."+self.corpus+".edges.generic.gz"
 		
 		self.lexical_cpos = parameters["lexical_cpos"]
 		
-		self.minimum_generic_frequency = parameters["min_generic"]
 		self.minimum_struct_frequency = parameters["min_struct"]
+		
+		self.ver = parameters["name"]
 	
 	@lru_cache(maxsize=8192)
 	def get_node (self, lma, cpos):
-		n = self.graph.run("MATCH (a) WHERE a.user= '"+self.user+"' AND a.lemma = '"+lma+"' AND a.pos = '"+cpos+"' return a", lemma = lma).evaluate()
+		n = None
+		try:
+			n = self.graph.run("MATCH (a) WHERE a.user= '"+self.user+"' AND a.lemma = '"+lma+"' AND a.pos = '"+cpos+"' return a", lemma = lma).evaluate()
+		except:
+			print ("problema ricerca nodo: {}".format(lma))	
 		return n	
 		
 	def remove_isolated(self):
@@ -52,117 +57,94 @@ class GraphBuilder:
 		with gzip.open(self.vocab_file, "rt") as f:
 			
 			for line in f:
-				if not i%1000000:
+				#~ if not i%1000000:
+				if not i%10000:
 					if not first:
 						tx.commit()
 					first = False
 					tx = self.graph.begin()
 					print(i)
-				
-				if not (line[0] == "_" or line[0]=="*"):
 
-					linesplit = line.strip().rsplit("\t")
+				linesplit = line.strip().rsplit("\t")
 					
-					#~ print (linesplit)
+				lemmasplit = linesplit[0].rsplit("/", 1)
+				#~ lemma = lemmasplit[0]
+				#~ lemma = re.escape(lemmasplit[0])
+				lemma = lemmasplit[0]
+				if "'" in lemma or '"' in lemma:
+					lemma = re.escape(lemma)
+					#~ print(lemma)
 					#~ input()
-					lemmasplit = linesplit[0].rsplit("/", 1)
-					lemma = lemmasplit[0]
-					pos = lemmasplit[1]
+				pos = lemmasplit[1]
 
-					fr = int(linesplit[1])
-					if pos in self.lexical_cpos and fr>self.minimum_struct_frequency:
-						#~ print(lemma + " " + pos)
-						#~ input()
-						n = Node("Lemma", lemma = lemma, pos = pos[0], frequency = fr, user = "Ludovica", ver = "prova1")
-						#~ print("created "+str(n))
-						#~ self.total_nouns+=fr
-						tx.create(n)
+				fr = int(linesplit[1])
+				if pos in self.lexical_cpos and fr>self.minimum_struct_frequency:
+					#~ print(lemma + " " + pos)
+					#~ input()
+					n = Node("Lemma", lemma = lemma, pos = pos[0], frequency = fr, user = "Ludovica", ver = self.ver)
+					#~ print("created "+str(n))
+					#~ self.total_nouns+=fr
+					tx.create(n)
 				i+=1
-		tx.commit()		
-	
-	def load_edges_generic (self):
-		lineno = 0
-		tx = self.graph.begin()
-		with gzip.open(self.edges_generic_file, "rt") as f:
-			for line in f:
-				linesplit = line.strip().split("\t")
-				
-				lemmi = linesplit[0].split()
-				labels = linesplit[1]
-				fr = int(linesplit[2])
-		
-				lineno+=1
-				if not lineno%100000:
-					print(lineno)
-					
-				if all (x[0] not in ["_", "*"] for x in lemmi) and fr > self.minimum_generic_frequency:
 
-					lemma1split = lemmi[0].rsplit("/", 1)
-					lemma2split = lemmi[1].rsplit("/", 1)
-					
-					try:
-						node1 = self.get_node(lemma1split[0], lemma1split[1][0])
-						node2 = self.get_node(lemma2split[0], lemma2split[1][0])
-						genassoc = Relationship (node1, "generic", node2, user = self.user, frequency = fr, MI = 0)
-						
-						tx.create(genassoc)
-					except Exception as e:
-						print("problema creazione nodo: {}".format(line))
-						#~ print(e)
-						print(str(node1)+" --- "+str(node2))
-						#~ input()
-						
-		tx.commit()
+		#~ n = Node("Lemma", lemma = "*", pos = "N", frequency = 1000000, user = "Ludovica", ver = "prova1")
+		#~ tx.create(n)
+		#~ n = Node("Lemma", lemma = "*", pos = "J", frequency = 1000000, user = "Ludovica", ver = "prova1")
+		#~ tx.create(n)
+		#~ n = Node("Lemma", lemma = "*", pos = "V", frequency = 1000000, user = "Ludovica", ver = "prova1")
+		#~ tx.create(n)
+		tx.commit()		
+
 		
 	def load_edges_deprels (self):
 		lineno = 0
 		tx = self.graph.begin()
-		with gzip.open(self.edges_file, "rt") as f:
-			for line in f:
-				linesplit = line.strip().split("\t")
+		#~ with gzip.open(self.edges_file, "rt") as f:
+		with open(self.edges_file, "rt") as f:
+			line = f.readline()
+			linesplit = line.strip().split("\t")
 				
+			lemmi = linesplit[0].split()
+			labels = linesplit[1].split("|")
+			fr = int(linesplit[2])
+			
+			while fr > self.minimum_struct_frequency:
+				lineno+=1
+				if not lineno%10000:
+					print(lineno)
+					
+				#~ try:
+				#~ if any(x[0] not in ["_", "*"] for x in lemmi):
+				args = {"user":self.user, "frequency":fr, "text": linesplit[0]+"---"+linesplit[1]}
+				
+				nodeslist = []
+				for i, l in enumerate(lemmi):
+					lsplit = l.rsplit("/", 1)
+					pos = lsplit[1]
+					
+					l = lsplit[0]
+					if "'" in l or '"' in l:
+						l = re.escape(l)
+					
+					lab = labels[i]
+					nodo = self.get_node(l, pos[0])
+					if nodo:
+						nodeslist.append((nodo, lab))
+				
+				
+				nodo_sr = Node("Struct", **args)
+				
+				tx.create(nodo_sr)
+				for nodo, lab in nodeslist:
+					r = Relationship(nodo, lab, nodo_sr, user=self.user, MI = 0)
+					tx.create(r)
+
+				line = f.readline()				
+				linesplit = line.strip().split("\t")
+					
 				lemmi = linesplit[0].split()
 				labels = linesplit[1].split("|")
 				fr = int(linesplit[2])
-		
-				lineno+=1
-				if not lineno%100000:
-					print(lineno)
-					
-				try:
-					if any(x[0] not in ["_", "*"] for x in lemmi) and fr > self.minimum_struct_frequency:
-						args = {"user":self.user, "frequency":fr, "text": linesplit[0]+"---"+linesplit[1]}
-						
-						nodeslist = []
-						for i, l in enumerate(lemmi):
-							if l[0] in ["_", "*"]:
-								args[labels[i]] = l
-							
-							else:
-								lsplit = l.rsplit("/", 1)
-								pos = lsplit[1]
-								
-								l = lsplit[0]
-								lab = labels[i]
-								nodo = self.get_node(l, pos[0])
-								nodeslist.append((nodo, lab))
-						
-						
-						nodo_sr = Node("Struct", **args)
-						
-						tx.create(nodo_sr)
-						for nodo, lab in nodeslist:
-							r = Relationship(nodo, lab, nodo_sr, user=self.user, MI = 0)
-							tx.create(r)
-							
-				
-				except Exception as e:
-					print ("problema creazione nodo: {}".format(line))
-					print(e)
-					#~ input()
-					#~ print(node1)
-					#~ print(node2)
-					#~ input()
 
 		tx.commit()
 
@@ -234,17 +216,18 @@ class GraphBuilder:
 				#~ query+=" and degree="+str(i+1)+" return x, s"
 				query+=" and degree="+str(i)+" return s"
 				
-				#~ print(query)
+				print(query)
 				#~ input()
 				
 				new_structs = self.graph.run(query)
 				
+				print (new_structs)
 				list_to_delete_asap = []
 				while new_structs.forward():
 					new_structs_curr = new_structs.current()
 					list_to_delete_asap.append((new_structs_curr["s"], new_structs_curr["s"]["text"]))
 				
-				
+				print (list_to_delete_asap)
 				struct_bare = sorted(list_to_delete_asap, key = lambda x: len(x[1]))[0][0]
 				
 				mi = f_curr_struct*math.log(self.len_corpus*f_curr_struct/(f_tok*struct_bare["frequency"]), 2) 
@@ -298,6 +281,7 @@ if __name__ == "__main__":
 	
 	graph = GraphBuilder(parameters)
 	graph.load_graph(sys.argv[2], sys.argv[3])
+	print ("graph reset")
 	graph.reset_graph()
 	print ("load vertices")
 	graph.load_vertices()
@@ -311,5 +295,3 @@ if __name__ == "__main__":
 	graph.sum_vocabulary()
 	print ("update lmi")
 	graph.update_lmi()
-	
-	#~ graph.load_edges_generic()
